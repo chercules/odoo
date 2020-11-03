e889e3482c2208e55e67f703395678f9a22f4c6f
chercules <carlosherculesg@hotmail.com>
2020-11-02 20:43:00 -0600

    Agrega Ejercicios de Python para odoo

---
+++ Estructuras_Control.py
@@ -0,0 +1,8 @@
+precio = 50
+if precio > 100:
+    precio = precio - 20
+elif precio == 50:
+    precio += 25
+else:
+    precio = precio + 20
+print(precio)
---
+++ Variables.py
@@ -0,0 +1,3 @@
+numero=100
+lista_a=[1,"Computadora","Clase Pyton","Dismo",5]
+print(numero," ",lista_a)
